c7f6976aa0441eca1db620344bab6ed2b7b0bba4
Twear81 <48881564+Twear81@users.noreply.github.com>
2024-03-09 08:39:36 +0100

    Update newerVersionApp.py

--- newerVersionApp.py
+++ newerVersionApp.py
@@ -17,6 +17,30 @@ for app in data['apps']:
     if 'versions' in app:
         app['versions'] = app['versions'][:1]
 
+# Créer la structure de données dans le nouveau format
+new_data = {
+    "META": {
+        "repoName": "swaggyP36000 IPA Library By Twear",
+        "repoIcon": "https://cdn3.iconfinder.com/data/icons/ultimate-social/150/17_google_play-512.png"
+    },
+    "Tweaked": []
+}
+
+# Convertir les applications
+for app in data["apps"]:
+    game = {
+        "name": app["name"],
+        "version": app["versions"][0]["version"],
+        "down": app["versions"][0]["downloadURL"],
+        "category": "Tweaked Apps",
+        "description": app["versions"][0]["localizedDescription"],
+        "bundleID": app["bundleIdentifier"],
+        "appstore": app["bundleIdentifier"],  # Cela peut nécessiter une mise à jour en fonction de la logique réelle
+        "icon": app["iconURL"]  # Lien vers l'icône ajouté
+    }
+    new_data["Tweaked"].append(game)
+
+
 # Écrire les données modifiées dans le fichier de sauvegarde
 with open(output_file_path, 'w') as file:
-    json.dump(data, file, indent=4)
+    json.dump(new_data, file, indent=4)
